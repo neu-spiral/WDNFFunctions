b9210da7703d1a7d9e0cd83bf463fdea79739f13
gozde-ozcan <gozcan@ece.neu.edu>
2020-02-11 19:53:50 -0500

    new codes and results

---
+++ code/plotter.py
@@ -0,0 +1,147 @@
+import matplotlib.pyplot as plt
+import argparse
+from matplotlib.transforms import Bbox
+import numpy as np
+from matplotlib.dates import date2num
+import datetime
+import os
+
+
+
+colors =['b', 'g', 'r', 'c' ,'m' ,'y' ,'k' ,'w']
+hatches = ['////', '/', '\\', '\\\\', '-', '--', '+', '']
+
+
+Algorithms = ['SE-CU','CU-SE','SE-Greedy','CG-RS500','CGT1','CGT2']
+graph2lbl =  {'erdos_renyi':'ER','erdos_renyi2':'ER-20Q','hypercube':'HC','hypercube2':'HC-20Q'}
+    
+Graphs = ['erdos_renyi','erdos_renyi2','star','hypercube', 'hypercube2','dtelekom','abilene','geant']
+
+
+def bar_ex1(DICS,outfile):
+    def form_vals(dic):
+        vals = []
+        lbls = []
+        for key in Graphs:
+            vals.append(dic[key])
+            if key in graph2lbl:
+                key = graph2lbl[key]
+            lbls.append(key)
+        return vals,lbls
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(18, 6)
+    width = 1
+    N = len(Graphs)
+    numb_bars = len(Algorithms)+1
+    ind = np.arange(0,numb_bars*N ,numb_bars)
+    RECTS = []
+    i = 0
+    for alg in Algorithms:
+        RECTS+= ax.bar(ind+i*width, form_vals(DICS[alg])[0], align='center',width=width, color = colors[i], hatch = hatches[i],label=alg,log=True)
+        i+=1
+    '''Set legend'''
+    LGD = ax.legend([alg for alg in Algorithms], ncol=len(Algorithms),borderaxespad=0.,loc=3, bbox_to_anchor=(0., 1.02, 1., .102),fontsize=15,mode='expand')
+
+    lbls =  form_vals(DICS[alg])[1]
+    
+    ax.set_xticks(ind+width*3) 
+    ax.set_xticklabels(tuple(lbls),fontsize = 16)
+
+    if args.mode2 == 'OBJ':
+        y_label = args.mode
+    else:
+        y_label = 'Time'
+    ax.set_ylabel(y_label,fontsize=22)
+    plt.yticks(fontsize = 18)
+    plt.xlim([ind[0]-width,ind[-1]+len(Algorithms)*width])
+    '''set axis range'''
+    if args.mode2 == 'TIME':
+        plt.ylim(0.003,80000)
+    else:
+        plt.ylim(50,10000)
+
+    fig.savefig(outfile+".pdf",format='pdf', bbox_extra_artists=(LGD,) )
+
+    plt.show() 
+
+def read_file(fname,normalize=0):
+    f = open(fname,'r')
+    l= eval(f.readline())
+    f.close()
+    (Time, OBJ) = l[-1] 
+    return {"TIME":Time,"OBJ":OBJ}
+
+def CG_readfile(CG_file,rounded_file):
+    dic_CG = read_file(CG_file)
+    dic_round = read_file(rounded_file)
+    return {"TIME":dic_CG["TIME"]+dic_round["TIME"],"OBJ":dic_round["OBJ"]}
+
+
+if __name__=="__main__":
+    
+    parser = argparse.ArgumentParser(description = 'Generate plots comparing different algorithms.',
+                                     formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--plot_type', default = 'bar', type = str, choices = ['bar', 'line'])
+    parser.add_argument('--mode',default='Cost', type=str,choices=['Gain','Cost'],help='Determine whether to plot gain or cost')
+    parser.add_argument('--normalize',action='store_true',help='Pass to normalize the plot.')
+    parser.add_argument('--mode2',default='OBJ', type=str,choices=['OBJ','TIME'],help='Determine whether to plot time or objective')
+    args = parser.parse_args()
+    
+    title = "Power law arrival distribution"
+    DICS = {"SE-CU":{}, 'CU-SE':{}, 'CG-RS500':{}, 'CGT1':{}, 'CGT2':{}, 'SE-Greedy':{}}
+    for graph in Graphs:
+       
+        if graph == "erdos_renyi2" or graph == "hypercube2":
+            problem_instance = "problem_%s_1000demands_100catalog_size_2mincap_2maxcap_100_powerlaw" %(graph[0:-1]) + "_rate1.0_20qnodes"
+        else: 
+            problem_instance = "problem_%s_1000demands_100catalog_size_2mincap_2maxcap_2_100_powerlaw" %(graph) + "_rate1.0_4qnodes"
+    
+        random_1 = "RANDOM/" + problem_instance + "-service_first"
+        random_2  = "RANDOM/"+ problem_instance +"-item_first"
+        greedy_f = "GREEDY/" + problem_instance
+        CG_1 = "OUTPUT/"+ problem_instance + "-500-samples-1000-iters" 
+        round_1 = "ROUNDED/"+ problem_instance + "-500-samples-1000-iters"
+        CG_2 = "OUTPUT/"+ problem_instance + "-taylor-k-1-1000-iters"
+        round_2 = "ROUNDED/"+ problem_instance + "-taylor-k-1-1000-iters"
+        CG_3 = "OUTPUT/"+ problem_instance + "-taylor-k-2-1000-iters"
+        round_3 = "ROUNDED/"+ problem_instance + "-taylor-k-2-1000-iters"
+
+        mode2 = args.mode2
+        if mode2 == 'TIME':
+            C0 = 0
+            SGN = +1
+        else:
+            if args.mode == 'Cost':
+                f_c0 = open(random_1, 'r')
+                OBJ0 = eval(f_c0.readline())[0]
+                f_c0.close()
+                C0 = OBJ0
+                SGN = -1
+
+            else:
+                C0 = 0
+                SGN = +1
+
+        if args.normalize:
+            RAN_VAL = C0 + SGN*read_file(random_1)[mode2]
+        else:
+            RAN_VAL = 1
+
+        DICS['SE-CU'][graph] = (C0 + SGN*read_file(random_1)[mode2])/RAN_VAL
+        DICS['CU-SE'][graph] = (C0 + SGN * read_file(random_2)[mode2]) / RAN_VAL
+        DICS['CG-RS500'][graph] = (C0 + SGN*CG_readfile(CG_1,round_1)[mode2] )/RAN_VAL
+        DICS['CGT1'][graph] = (C0 + SGN * CG_readfile(CG_2, round_2)[mode2]) / RAN_VAL
+        DICS['CGT2'][graph] = (C0 + SGN*CG_readfile(CG_3,round_3)[mode2])/RAN_VAL
+        DICS['SE-Greedy'][graph] = (C0 +SGN*read_file(greedy_f)[mode2])/RAN_VAL
+
+    print DICS
+
+    if args.mode2 == 'TIME':
+        dir = "TIME/"
+    else:
+        dir = "OBJ/"
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    outfile = dir + "problem_1000demands_300catalog_size_mincap_2maxcap_2_100_powerlaw"
+    bar_ex1(DICS,outfile)
---
+++ code/rounding.py
@@ -0,0 +1,139 @@
+import random
+from random_replacement import ro_uvr,OBJ_dict
+from cvxopt import matrix
+import time
+import argparse
+from pyspark import SparkContext, SparkConf
+from helpers import write
+from Toplogy_gen import Problem, Demand
+import os
+import numpy as np
+
+def SwapRound(beta_bases_tpl,v):
+     
+    def MergeBases(beta1, B1, beta2, B2,random_generator):
+        def subtract(B1,B2):
+            """Return B1-B2"""
+            a= set(B1)
+            b=set(B2)
+            return tuple(a.difference(b))
+        k=0
+        while set(B1)!=set(B2):
+            B1_slsh_B2 = subtract(B1,B2)
+            B2_slsh_B1 = subtract(B2,B1)
+            i = B1_slsh_B2[0]
+            j = B2_slsh_B1[0]
+            RN = random_generator.random()
+            if RN<beta1/(beta1+beta2):
+                B2 = subtract(B2,(j,))+(i,)
+            else:
+                B1 = subtract(B1,(i,))+(j,)
+            k+=1
+        return B1
+    RG = random.Random()
+    RG.seed(v)
+    p = len(beta_bases_tpl)
+    C_new = beta_bases_tpl[0][0]
+    beta1 = beta_bases_tpl[0][1]
+    for k in range(p-1):
+        beta2 = beta_bases_tpl[k+1][1]
+        B2 = beta_bases_tpl[k+1][0]
+        C_old = C_new
+         
+        C_new = MergeBases(beta1=beta1,B1=C_old,beta2=beta2,B2=B2,random_generator=RG)
+        beta1 = beta1 +beta2
+    return C_new
+
+
+def distributed_rounding(input,P):
+    def flatten_dictionary(dict):
+        l = []
+        for v in dict:
+           l.append((v,dict[v]))
+        return l
+    def make_unique(l):
+        l.sort()
+        return tuple(l)
+    f = open(input, 'r')
+    base_list= eval(f.readline())
+    f.close()
+    conf = (SparkConf()
+         .setMaster("local[40]")
+         .setAppName("My app")
+         .set("spark.executor.memory", "100g"))
+    sc = SparkContext(conf=conf)
+    sc.setLogLevel("ERROR")
+    RDD_bases = sc.parallelize(base_list).flatMap(flatten_dictionary).mapValues(lambda base_list:make_unique(base_list))\
+                                         .map(lambda (v,tpl_base):((v,tpl_base),1)).reduceByKey(lambda x,y:x+y, P)\
+                                         .map(lambda ((v,base_tpl),cnt):(v,[(base_tpl,cnt)])).reduceByKey(lambda x,y:x+y, P)\
+                                         .map(lambda (v,base_tpls):(v,(base_tpls,v))).partitionBy(P)\
+             			         .mapValues(lambda (base_tpls,v):SwapRound(base_tpls,v)).cache()
+                                         
+    return RDD_bases 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = 'Simulate the Swap Rounding Alg.',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('OBJ', type=str, help='Objective function queuing type', choices=['MMInfty', 'Info'])
+    parser.add_argument('SOLUTION', type=str, help='Solution queuing type', choices=['MMInfty', 'Info'])
+    parser.add_argument('--graph_type', default="erdos_renyi", type=str, help='Graph type',
+                        choices=['erdos_renyi', 'balanced_tree', 'hypercube', "cicular_ladder", "cycle", "grid_2d",
+                                 'lollipop', 'expander', 'hypercube', 'star', 'barabasi_albert', 'watts_strogatz',
+                                 'regular', 'powerlaw_tree', 'small_world', 'geant', 'abilene', 'dtelekom',
+                                 'servicenetwork'])
+    parser.add_argument('--query_nodes', default=10, type=int, help='Number of nodes generating queries')
+
+    parser.add_argument('--order_moment', default=2, type=int, help='Order of moment for the expected cost', choices=[1,2,3,4])
+    parser.add_argument('--estimator',default='sample',choices=['sample','taylor','power','DR'],help='Type of estimator')
+    parser.add_argument('--k',type=int, default=1,help="Order of taylor expansion")
+    args  = parser.parse_args()
+    problem_instance = "problem_" + args.graph_type + "_1000demands_100catalog_size_2mincap_2maxcap_100size_powerlaw_rate1.0_" + str(args.query_nodes) + "qnodes_" + str(args.order_moment) + "order_" + args.SOLUTION
+    if args.estimator == "sample":
+        input = problem_instance + "-500-samples-1000-iters"
+    elif args.estimotor == "taylor":
+        input = problem_instance + "-taylor-k-" + str(args.k)+ "-1000-iters"
+    else:
+        input = problem_instance
+    OBJ = "problem_" + args.graph_type + "_1000demands_100catalog_size_2mincap_2maxcap_100size_powerlaw_rate1.0_" + str(args.query_nodes) + "qnodes_" + str(args.order_moment) + "order_" + args.OBJ
+
+    input = "BASE/"+ input
+    input_problem = "INPUT_NEW/"+problem_instance
+    obj = "INPUT/" + OBJ
+    P = Problem.unpickle_cls(input_problem)
+    P_obj = Problem.unpickle_cls(obj)
+    capacities = P.capacities
+    dem_items=[]
+    for demand in P.demands:
+         item = demand.item
+         dem_items.append(item)
+    V,I = (len(P.capacities),len(set(dem_items)))
+    RDD_bases = distributed_rounding(input,56)
+    base_out  = RDD_bases.collect()
+    X = np.zeros([V,I])
+    tstart = time.time()
+    for (v,items) in base_out:
+         for item in items:
+             X[v,item] = 1
+    print X
+    ro_uvr_dic = ro_uvr(P,X)
+    OBJ =  OBJ_dict(P_obj,ro_uvr_dic)
+    elapsed = time.time()-tstart
+    print "OBJ is %f and time taken is %f" %(P.cost-OBJ,elapsed)
+    track = [P.cost]
+    track += [(elapsed,P.cost-OBJ)]
+    track += [X]
+
+    dir_rounded = "ROUNDED/"
+    if not os.path.exists(dir_rounded):
+        os.mkdir(dir_rounded)
+
+    if args.OBJ == args.SOLUTION:
+        output = input
+    else:
+        if args.estimator == "sample":
+            output = problem_instance + "2-500-samples-1000-iters"
+        elif args.estimotor == "taylor":
+            output = problem_instance + "2-taylor-k-" + str(args.k) + "-1000-iters"
+        else:
+            output = problem_instance + "2"
+
+    rounded = dir_rounded + output
+    np.save(rounded, track)
